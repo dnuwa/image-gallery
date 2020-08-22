b0caaeb522f707fec1baffe40f685f4379a3e371
dnuwa <daniel.nuwa@gmail.com>
2020-08-22 19:59:10 +0300

    -installs and configures some popular React libraries (like Redux and Redux Saga), - uses styled-components to help you manage the CSS of your React components - configures React Bootstrap to give you a responsive, mobile-first application - uses Auth0 to handle authentication easily

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# image-gallery
-Un-splash api image gallery
+# Beyond Create React App: React Router, Redux Saga, and More
+
+just a quick run around Redux, Redux-saga, styled-components, Auth0 to handle authentication
+
---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import Container from 'react-bootstrap/Container';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+
+import AddToDo from './containers/AddToDo';
+import ToDoListContainer from './containers/ToDoListContainer';
+import { Route } from 'react-router-dom';
+import Navigation from './components/Navigation';
+import Callback from './containers/Callback';
+import NavigationContainer from './containers/NavigationContainer';
+
+class App extends Component {
+  render() {
+    return (
+      <Container>
+        <Row className="row">
+          <Col xs={12}>
+            <h1>To Do List</h1>
+            <NavigationContainer />
+            <Route exact path="/" component={ToDoListContainer} />
+            <Route exact path="/new-item" component={AddToDo} />
+            <Route exact path="/callback" component={Callback} />
+          </Col>
+        </Row>
+      </Container>
+    );
+  }
+}
+
+export default App;
